6db911857f08d169cd3331878972a30172b78912
SadRus <rystixx@yandex.ru>
2022-11-20 00:50:20 +0300

    publish photo to Telegram channel.

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ from dotenv import load_dotenv
 if __name__ == '__main__':
     load_dotenv()
     bot = telegram.Bot(token=os.environ['TG_BOT_TOKEN'])
-    bot.send_message(text='hi', chat_id=440084749)
+    # bot.send_message(text='hi', chat_id=440084749)
+    bot.send_photo(chat_id=-809390367, photo=open('images/nasa_apod1.jpg', 'rb'))
